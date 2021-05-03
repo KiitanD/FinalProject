70cc4636efd83b7579389cc820f57512b6e66939
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-05-03 15:33:50 +0000

    add function for sorting cylindrical blocks by surface area

--- Projectbase.cpp
+++ Projectbase.cpp
@@ -95,3 +95,7 @@ bool greaterDiameter(sphericalBlocks block1, sphericalBlocks block2) {
     return (block1.diameter > block2.diameter);
 }
 
+//function to sort cylindricalBlocks array based on surface area
+bool greaterSurfaceArea(cylindricalBlocks block1, cylindricalBlocks block2) {
+    return (block1.surface_area > block2.surface_area);
+}
