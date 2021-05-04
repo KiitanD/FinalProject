ad29f2fff66fdbd58f410bcf89bfda4ae86140de
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-05-04 05:14:29 +0000

    create header files for createCylinderArray.cpp and createSphereArray.cpp

---
+++ createCylinderArray.h
@@ -0,0 +1,12 @@
+//
+// Created by excel on 04/05/2021.
+//
+
+#ifndef FINALPROJECT_CREATECYLINDERARRAY_H
+#define FINALPROJECT_CREATECYLINDERARRAY_H
+
+//comparator function to sort cylindricalBlocks array based on surface area
+bool greaterSurfaceArea(cylindricalBlocks block1, cylindricalBlocks block2);
+
+void sortAndPrintCylinder(vector<cylindricalBlocks> cylinderObject);
+#endif //FINALPROJECT_CREATECYLINDERARRAY_H
---
+++ createSphereArray.h
@@ -0,0 +1,13 @@
+//
+// Created by excel on 04/05/2021.
+//
+
+#ifndef FINALPROJECT_CREATESPHEREARRAY_H
+#define FINALPROJECT_CREATESPHEREARRAY_H
+
+//comparator function for sorting sphericalBlocks array based on diameter
+bool greaterDiameter(sphericalBlocks block1, sphericalBlocks block2);
+
+void sortAndPrintSphere(vector<sphericalBlocks> sphereObject);
+
+#endif //FINALPROJECT_CREATESPHEREARRAY_H
